5fd7e7c35cd356e093c26dd72a8720ffbbead100
MVRezenov <rezenov.micha@yandex.ru>
2017-04-25 15:37:28 +0500

    functions Draw

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,16 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Draw( 1, 3, '*' );
+            Draw( 4, 5, '#');
+
             Console.ReadLine();
+        }
+
+    static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
