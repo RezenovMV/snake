57dcf241437a8bbb40b34383582e13d1976b15d2
MVRezenov <rezenov.micha@yandex.ru>
2017-04-26 13:55:55 +0500

    управляемая змейка

--- snake/snake/Snake.cs
+++ snake/snake/Snake.cs
@@ -40,5 +40,17 @@ namespace snake
             nextPoint.Move(1, direction);
             return nextPoint;
         }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.Left;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.Right;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.Up;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.Down;
+        }
     }
 }
